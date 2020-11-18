755bd844d78bc9104750da40c1b26d3d075d7c73
The_count <thescheremetev@mail.ru>
2020-11-18 23:20:42 +0300

    first conmmit

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ var bgline = document.querySelector('.burger-line')
 var menu = document.querySelector('.menu.mob');
 var menuitem = document.querySelector(".mob");
 
+// show and hide mobile version menu
 bgmenu.addEventListener('click', menus);
 function menus() {
   bgline.classList.toggle('activ');
   menu.classList.toggle('show');
 };
 
+// hide menu by click for link
 menuitem.addEventListener('click', closemenu);
 function closemenu(){
   menu.classList.remove("show");
