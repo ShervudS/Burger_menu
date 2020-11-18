3cbe3c91ac3844ee0fc1013ceaa7dc1cd7b12b90
The_count <thescheremetev@mail.ru>
2020-11-18 19:02:55 +0300

    first conmmit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var bgmenu = document.querySelector('.burger-menu');
 var bgline = document.querySelector('.burger-line')
+var menu = document.querySelector('.menu.mob')
 
 bgmenu.addEventListener('click', myFunction);
 
